f726cb24592d4fc4798c995593083549fec70ef7
Papierz <ppp16@vp.pl>
2018-03-04 18:39:32 +0100

    add test listview

--- app/src/main/java/com/example/piotr/projectmanager/NewProjectActivity.java
+++ app/src/main/java/com/example/piotr/projectmanager/NewProjectActivity.java
@@ -5,7 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NewProjectActivity extends AppCompatActivity {
 
@@ -15,6 +35,39 @@ public class NewProjectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_project);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+        final ListView lv = (ListView) findViewById(R.id.listViewContributors);
+        final Button btn = (Button) findViewById(R.id.buttonAddContributor);
 
+        // Initializing a new String Array
+        String[] fruits = new String[] {
+                "Cape Gooseberry",
+                "Capuli cherry"
+        };
+
+        // Create a List from String Array elements
+        final List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
+
+        // Create an ArrayAdapter from List
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, fruits_list);
+
+        // DataBind ListView with items from ArrayAdapter
+        lv.setAdapter(arrayAdapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add new Items to List
+                fruits_list.add("Loquat");
+                fruits_list.add("Pear");
+                /*
+                    notifyDataSetChanged ()
+                        Notifies the attached observers that the underlying
+                        data has been changed and any View reflecting the
+                        data set should refresh itself.
+                 */
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+    }
 }
