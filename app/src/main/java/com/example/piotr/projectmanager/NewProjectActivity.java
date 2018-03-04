5a1ec4aabd6428eb5a38d9e55d347e4980544622
Papierz <ppp16@vp.pl>
2018-03-04 19:18:13 +0100

    viewList change heigth when add item

--- app/src/main/java/com/example/piotr/projectmanager/NewProjectActivity.java
+++ app/src/main/java/com/example/piotr/projectmanager/NewProjectActivity.java
@@ -7,10 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,44 +30,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NewProjectActivity extends AppCompatActivity {
-
+    int totalHeight = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_project);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         final ListView lv = (ListView) findViewById(R.id.listViewContributors);
         final Button btn = (Button) findViewById(R.id.buttonAddContributor);
-
-        // Initializing a new String Array
-        String[] fruits = new String[] {
-                "Cape Gooseberry",
-                "Capuli cherry"
-        };
-
-        // Create a List from String Array elements
-        final List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
-
-        // Create an ArrayAdapter from List
+        final List<String> contributors_list = new ArrayList<String>();
+        final EditText editText = (EditText) findViewById(R.id.editTextIdContributor);
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, fruits_list);
-
-        // DataBind ListView with items from ArrayAdapter
+                (this, android.R.layout.simple_list_item_1, contributors_list);
         lv.setAdapter(arrayAdapter);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Add new Items to List
-                fruits_list.add("Loquat");
-                fruits_list.add("Pear");
-                /*
-                    notifyDataSetChanged ()
-                        Notifies the attached observers that the underlying
-                        data has been changed and any View reflecting the
-                        data set should refresh itself.
-                 */
+                totalHeight+=v.getMeasuredHeight();
+                contributors_list.add(editText.getText().toString());
+                RelativeLayout.LayoutParams mParam = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,(int)30);
+                ViewGroup.LayoutParams params = lv.getLayoutParams();
+                params.height=totalHeight+(lv.getDividerHeight()*(arrayAdapter.getCount()-1));
+                lv.setLayoutParams(params);
+                lv.requestLayout();
                 arrayAdapter.notifyDataSetChanged();
             }
         });
